1675040b16de2206b600a18e22be2fc819b0c7dd
Park Young Jun <june0313@gmail.com>
2021-01-30 15:33:28 +0900

    toBinary 함수를 꼬리재귀로 개선

--- src/main/kotlin/_3_recursion/binary.kt
+++ src/main/kotlin/_3_recursion/binary.kt
@@ -1,10 +1,17 @@
 package _3_recursion
 
 fun main() {
-    println(toBinary(127 ))
+    for (i in 0..100) {
+        println(toBinaryTail(i))
+    }
 }
 
 private fun toBinary(n: Int): String = when {
     n < 2 -> "${n % 2}"
     else -> "${toBinary(n / 2)}${(n % 2)}"
+}
+
+private tailrec fun toBinaryTail(n: Int, acc: String = ""): String = when {
+    n < 2 -> "${n % 2}$acc"
+    else -> toBinaryTail(n / 2, "${n % 2}$acc")
 }
