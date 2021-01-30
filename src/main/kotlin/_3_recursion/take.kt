af5c3d96d859f740003ac38645561e597c111e78
Young Jun Park <june0313@gmail.com>
2021-01-30 20:36:22 +0900

    take 함수를 꼬리재귀로 작성

--- src/main/kotlin/_3_recursion/take.kt
+++ src/main/kotlin/_3_recursion/take.kt
@@ -2,10 +2,17 @@ package _3_recursion
 
 fun main() {
     println(take(3, listOf(2, 3, 4, 5, 6)))
+    println(takeTail(3, listOf(2, 3, 4, 5, 6)))
 }
 
 private fun take(n: Int, list: List<Int>): List<Int> = when {
     n <= 0 -> listOf()
     list.isEmpty() -> listOf()
     else -> listOf(list.first()) + take(n - 1, list.drop(1))
+}
+
+private fun takeTail(n: Int, list: List<Int>, acc: List<Int> = listOf()): List<Int> = when {
+    0 >= n -> acc
+    list.isEmpty() -> acc
+    else -> takeTail(n - 1, list.tail(), acc + list.head())
 }
