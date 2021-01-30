274794daf6d5f114896d66fb458a2f3f7347bdf1
Park Young Jun <june0313@gmail.com>
2021-01-30 16:58:35 +0900

    replicate 함수를 꼬리재귀로 최적화

--- src/main/kotlin/_3_recursion/replicate.kt
+++ src/main/kotlin/_3_recursion/replicate.kt
@@ -3,6 +3,8 @@ package _3_recursion
 fun main() {
     println(replicate(3, 5))
     println(replicate(7, 5))
+    println(replicateTail(7, 5))
+    println(replicateTail(10, 13))
 }
 
 private fun replicate(n: Int, element: Int): List<Int> {
@@ -10,4 +12,9 @@ private fun replicate(n: Int, element: Int): List<Int> {
         1 -> listOf(element)
         else -> listOf(element) + replicate(n - 1, element)
     }
+}
+
+private tailrec fun replicateTail(n: Int, element: Int, acc: List<Int> = listOf()): List<Int> = when (n) {
+    0 -> acc
+    else -> replicateTail(n - 1, element, acc + listOf(element))
 }
